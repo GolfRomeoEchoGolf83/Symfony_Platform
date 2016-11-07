8e45c531d4515762474f43473f3bc9c9cb93366c
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-11-07 15:37:01 +0100

    Modifying Controller and creating views related to it.

--- src/Greg/PlatformBundle/Controller/AdvertController.php
+++ src/Greg/PlatformBundle/Controller/AdvertController.php
@@ -6,7 +6,71 @@
  * Date: 07/11/2016
  * Time: 15:16
  */
-class AdvertController
-{
 
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class AdvertController extends Controller
+{
+    /**
+     * @param $page
+     * @return mixed
+     */
+    public function indexAction($page)
+    {
+        if($page < 1)
+        {
+            throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
+        }
+        return $this->render('GregPlatformBundle:Advert:index.html.twig');
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function viewAction($id)
+    {
+        return $this->render('GregPlatformBundle:Advert:view.html.twig', array('id' => $id));
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function addAction(Request $request)
+    {
+        if($request->isMethod('POST'))
+        {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce enregistrée.');
+            return $this->redirectToRoute('greg_platform_view', array('id' => 5));
+        }
+        return $this->render('GregPlatformBundle:Advert:add.html.twig');
+    }
+
+    /**
+     * @param $id
+     * @param Request $request
+     * @return mixed
+     */
+    public function editAction($id, Request $request)
+    {
+        if($request->isMethode('POST'))
+        {
+            $request->getSession()->getFlashBag()->add('notice', 'annonce modifiée.');
+            return $this->redirectionToRoute('greg_platform_view', array('id' => 5));
+        }
+        return $this->render('GregPlatformBundle:Advert:edit.html.twig');
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function deleteAction($id)
+    {
+        return $this->render('GregPlatformBundle:Advert:delete.html.twig');
+    }
 }
