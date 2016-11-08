516bb4e93f9ec62e7a64f87e5d824d8f37fc6d51
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-11-08 14:53:25 +0100

    Updating AdvertController.php with listAdverts variable in indexAction()

--- src/Greg/PlatformBundle/Controller/AdvertController.php
+++ src/Greg/PlatformBundle/Controller/AdvertController.php
@@ -7,9 +7,10 @@
  * Time: 15:16
  */
 
-namespace OC\PlatformBundle\Controller;
+namespace Greg\PlatformBundle\Controller;
 
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
@@ -20,11 +21,39 @@ class AdvertController extends Controller
      */
     public function indexAction($page)
     {
+        // une page doit toujours être >= 1
         if($page < 1)
         {
+            // exception NotFoundHttpException : erreur 404
             throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
         }
-        return $this->render('GregPlatformBundle:Advert:index.html.twig', array('listAdverts'=>array()));
+        // récupère la liste des annonces avant de les passer au template
+        // liste d'annonce en dur
+        $listAdverts = array(
+            array(
+                'title'     => 'Recherche développeur Symfony',
+                'id'        => 1,
+                'author'    => 'Alexandre',
+                'content'   => 'Nous recherchons un développeur Symfony débutant.',
+                'date'      => new \DateTime()),
+
+            array(
+                'title'     => 'Mission de webmaster',
+                'id'        => 2,
+                'author'    => 'Hugo',
+                'content'   => 'Nous recherchons un webmaster capable de maintenir notre site.',
+                'date'      => new \DateTime()),
+
+            array(
+                'title'     => 'Offre de stage webdesigner',
+                'id'        => 3,
+                'author'    => 'Mathieu',
+                'content'   => 'Nous proposons un poste de webdesigner.',
+                'date'      => new \DateTime()),
+        );
+
+        // appel de template
+        return $this->render('GregPlatformBundle:Advert:index.html.twig', array('listAdverts'=>array('listAdverts' => $listAdverts)));
     }
 
     /**
@@ -33,6 +62,7 @@ class AdvertController extends Controller
      */
     public function viewAction($id)
     {
+        // récupère l'annonce correspondant à l'id $id
         $advert = array(
             'title'     => 'Recherche développeur Symfony',
             'id'        => $id,
@@ -49,11 +79,14 @@ class AdvertController extends Controller
      */
     public function addAction(Request $request)
     {
+        // si la requête est en POST, alors le visiteur a soumis le formulaire
         if($request->isMethod('POST'))
         {
+            // création et gestion du formulaire
             $request->getSession()->getFlashBag()->add('notice', 'Annonce enregistrée.');
             return $this->redirectToRoute('greg_platform_view', array('id' => 5));
         }
+        // si on n'est pas en POST alors on affiche le formulaire
         return $this->render('GregPlatformBundle:Advert:add.html.twig');
     }
 
@@ -64,6 +97,7 @@ class AdvertController extends Controller
      */
     public function editAction($id, Request $request)
     {
+        // récupère l'annonce correspondant à $id
         if($request->isMethode('POST'))
         {
             $request->getSession()->getFlashBag()->add('notice', 'annonce modifiée.');
@@ -86,16 +120,21 @@ class AdvertController extends Controller
      */
     public function deleteAction($id)
     {
+        // récupère l'annonce correspondant à $id
+
+        // gère la suppression de l'annonce
         return $this->render('GregPlatformBundle:Advert:delete.html.twig');
     }
 
     public function menuAction($limit)
     {
+        // sera modifié par la récupération des data en BDD
         $listAdverts = array(
             array('id' => 2, 'title' => 'Recherche développeur Symfony'),
             array('id' => 5, 'title' => 'Mission de webmaster'),
             array('id' => 9, 'title' => 'Offre de stage webdesigner')
         );
+        // le contrôleur passe les variables nécessaires au template
         return $this->render('GregPlatformBundle:Advert:menu.html.twig', array('listAdverts' => $listAdverts));
     }
 }
