416d363d3e1b8849fcb2cb85590197da4170fedc
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-11-14 16:51:27 +0100

    Creating CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Greg\PlatformBundle\GregPlatformBundle(),
+            new Greg\CoreBundle\GregCoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
