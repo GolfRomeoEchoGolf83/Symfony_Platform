d711afbd352ea1b955bf0d39112714f7517bc136
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-11-16 16:42:55 +0100

    Adding fixtures

---
+++ src/Greg/PlatformBundle/DataFixtures/ORM/LoadSkill.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Greg
+ * Date: 16/11/2016
+ * Time: 16:40
+ */
+
+namespace Greg\PlatformBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Greg\PlatformBundle\Entity\Skill;
+
+class LoadSkill implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        // Liste des noms de compétences
+        $names = array('PHP', 'Symfony', 'C++', 'Java', 'Photoshop', 'Blender', 'Bloc-note');
+        foreach ($names as $name) {
+            // On crée la compétence
+            $skill = new Skill();
+            $skill->setName($name);
+            // On la persiste
+            $manager->persist($skill);
+        }
+        // On déclenche l'enregistrement de toutes les compétences
+        $manager->flush();
+    }
+}
